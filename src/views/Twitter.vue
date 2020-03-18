bd2ebfd633f979305724f5fa19535ef49592d13b
Komichi_19 <ryolego125@gmail.com>
2020-03-18 17:37:26 +0900

    a

---
+++ src/views/Twitter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="Twitter">
+    <div>
+      <h1>xxpoitterxx</h1>
+      <input type="text" v-model="user" />
+      <input type="text" v-model="tweet" />
+      <button @click="Tweet">tweet</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "tweet",
+  data() {
+    return {
+      tweet: "",
+      user: ""
+    };
+  },
+  methods: {
+    Tweet() {
+      axios.post("/api/tweet", {
+        tweet: this.tweet,
+        user: this.user
+      });
+    }
+  }
+};
+</script>
